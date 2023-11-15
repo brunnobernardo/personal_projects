b09213f14cf7059eab16cfc981a206b1a42f9517
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-11-15 13:28:39 -0300

    latest size adjustments

--- fino_projeto_2.0/script.js
+++ fino_projeto_2.0/script.js
@@ -2,5 +2,10 @@ window.sr = ScrollReveal({ reset: true});
 
  sr.reveal('.scrollreveal', { 
     rotate: {x:40, y:0, z:0},
-    duration: 1300
+    duration: 1500
+});
+
+sr.reveal('.scrollreveal-2', { 
+    rotate: {x:40, y:0, z:0},
+    duration: 2000
 });
